bd00dd15c4fe9dc7af46986d73d7f39b3dec8b69
Jennifer Le <122183026+JenniferMLe@users.noreply.github.com>
2025-02-18 19:57:54 -0600

    edited procfile

--- init.R
+++ init.R
@@ -1,15 +1,8 @@
 # install core tidyverse packages
 install.packages("dplyr")
-install.packages("forcats")
 install.packages("ggplot2")
-install.packages("lubridate")
-install.packages("purrr")
 install.packages("readr")
-install.packages("stringr")
-install.packages("tibble")
 install.packages("tidyr")
-
-# install other packages
 install.packages("shiny")
 install.packages("shinythemes")
 install.packages("DT")
